9393f114a95e9539dc68da55a8bef35f16c014da
Lloyd <ljsayer@btopenworld.com>
2023-11-13 18:28:50 +0000

    update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,8 +1,7 @@
 import yaml
 from sqlalchemy import create_engine, inspect
 import pandas as pd
-import psycopg2
-
+import os
 
 class RDSDatabaseConnector:
     def __init__(self, yamlCredentials):
@@ -20,17 +19,17 @@ class RDSDatabaseConnector:
         engine.connect()
         return engine
 
-    def createDataframe(self, engine):
-        self.connector()
+    def createDataframe(self):
+        engine = self.connector()
         loan_payments = pd.read_sql_table('loan_payments', engine)
         return loan_payments
 
-    def saveData(self, loan_payments):
-        self.createDataframe
-        loan_payments.to_csv(r'C:\Users\ljsay\Documents\GitHub\exploratory-data-analysis---customer-loans-in-finance\loanpayments.csv')    
+    def saveData(self):
+        loan_payments = self.createDataframe()
+        loan_payments.to_csv('loanpayments.csv')    
 
 p1 = RDSDatabaseConnector('credentials.yml')
-p1.saveData
+p1.saveData()
         
 
 
@@ -43,7 +42,7 @@ p1.saveData
 
 
 #with open('credentials.yml', 'r') as file:
- #  yamlCredentials = yaml.safe_load(file)
+#  yamlCredentials = yaml.safe_load(file)
 #engine = create_engine(f"postgresql+psycopg2://{yamlCredentials['RDS_USER']}:{yamlCredentials['RDS_PASSWORD']}@{yamlCredentials['RDS_HOST']}:{yamlCredentials['RDS_PORT']}/{yamlCredentials['RDS_DATABASE']}")
 #engine.execution_options(isolation_level='AUTOCOMMIT').connect()
 #engine.connect()
@@ -51,3 +50,5 @@ p1.saveData
 #print(inspector.get_table_names())
 #loan_payments = pd.read_sql_table('loan_payments', engine)
 #loan_payments.head(10)
+
+#print(yamlCredentials['RDS_HOST'])
