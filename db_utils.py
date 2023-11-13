8ac435d72928192ec65f43551ae5ae8efb67d90e
Lloyd <ljsayer@btopenworld.com>
2023-11-13 13:06:48 +0000

    __init__ created

--- db_utils.py
+++ db_utils.py
@@ -1,2 +1,11 @@
+import yaml
+
 class RDSDatabaseConnector:
+    def __init__(self, yamlCredentials):
+        self.yamlCredentials = yamlCredentials
+    
+    def credentialsLoader():
+        with open('credentials.yaml', 'r') as file:
+            yamlCredentials = yaml.safe_load(file)
+
     
